4cc15df2061da594b577bb686aa01b918dea916d
Howard Wu <antiair.tw@gmail.com>
2017-08-05 14:55:10 +0800

    modify boston_house.py

--- boston_house.py
+++ boston_house.py
@@ -4,18 +4,20 @@
 #
 from sklearn import datasets
 from sklearn.linear_model import LinearRegression
-##import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
-loaded_data = datasets.load_boston()
-data_X = loaded_data.data
-data_y = loaded_data.target
+##loaded_data = datasets.load_boston()
+##data_X = loaded_data.data
+##data_y = loaded_data.target
+##
+##model = LinearRegression()
+##model.fit(data_X , data_y)
+##
+##print(model.predict(data_X[:4, :]))     #產生預測值
+##print(data_y[:4])                                       #實際值
 
-model = LinearRegression()
-model.fit(data_X , data_y)
+X, y = datasets.make_regression (n_samples = 100, n_features =1, n_targets =1, noise =1)
+plt.scatter(X,y)
+plt.show()
 
-print(model.predict(data_X[:4, :]))     #產生預測值
-print(data_y[:4])                                       #實際值
-
-##X, y = datasets.make_regression (n_samples = 100, n_features =1, n_targets =1, noise =1)
-##plt.scatter(X,y)
-##plt.show()
+#test github
